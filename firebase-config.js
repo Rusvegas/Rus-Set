96b1727f44d551a7778e7a44da5b9638539393f7
Rusvegas <foxy17727@gmail.com>
2024-05-18 05:29:36 +0000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAhz-x6t0T6Xh6odtM3Gb2s5HcnoBpLeyw",
+    authDomain: "rusvegas-b2dc1.firebaseapp.com",
+    projectId: "rusvegas-b2dc1",
+    storageBucket: "gs://rusvegas-b2dc1.appspot.com",
+    messagingSenderId: "724380564966",
+    appId: "1:724380564966:web:556130f541fd3d7bb8ea27"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
